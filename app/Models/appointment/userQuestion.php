5a9dbfe41c8af92487a1d0f3357d2eb5465707af
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-11-25 10:50:48 -0600

    userQuestion relationship

--- app/Models/appointment/userQuestion.php
+++ app/Models/appointment/userQuestion.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\appointment;
 
+use App\Models\catalogue\typeClass;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +12,9 @@ class userQuestion extends Model
     public function questionStudying()
     {
         return $this->hasMany(userStudying::class);
+    }
+    public function questionClass()
+    {
+        return $this->hasMany(typeClass::class);
     }
 }
--- app/Models/catalogue/typeClass.php
+++ app/Models/catalogue/typeClass.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\catalogue;
 
+use App\Models\appointment\userQuestion;
 use App\Models\appointment\userRenovation;
 use App\Models\appointment\userStudying;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -10,10 +11,14 @@ use Illuminate\Database\Eloquent\Model;
 class typeClass extends Model
 {
     use HasFactory;
-    protected $fillable = ['type_exam_id', 'name'];
+    protected $fillable = ['type_exam_id','user_question_id','name'];
     public function classExam()
     {
         return $this->belongsTo('App\Models\catalogue\typeExam', 'type_exam_id');
+    }
+    public function classQuestion()
+    {
+        return $this->belongsTo(userQuestion::class,'');
     }
     public function classStudying()
     {
