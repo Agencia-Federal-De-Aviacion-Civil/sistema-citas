80e0737938b7e388f5965e999aac898f047b0007
jessicasoto01afac <jessica.soto928@gmail.com>
2022-11-25 11:22:58 -0600

    mas cambios

--- resources/views/livewire/appointment/generate.blade.php
+++ resources/views/livewire/appointment/generate.blade.php
@@ -44,7 +44,7 @@
                                                 class="block mb-2 text-base font-medium text-gray-900 dark:text-white">INGRESA
                                                 LA REFERENCIA DE PAGO</label>
                                             <input type="text" x-ref="payment"
-                                                class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                                                class="py-3 px-4 block w-full font-bold	border-gray-200 rounded-md text-base focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
                                                 placeholder="ingresa la referencia de pago">
                                         </div>
 
