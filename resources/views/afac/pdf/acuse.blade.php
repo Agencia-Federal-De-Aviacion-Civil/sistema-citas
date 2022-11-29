f8707cb7486e285eb56a640046590daa5c03408e
jessicasoto01afac <jessica.soto928@gmail.com>
2022-11-28 18:00:20 -0600

    cambios

--- resources/views/afac/pdf/acuse.blade.php
+++ resources/views/afac/pdf/acuse.blade.php
@@ -12,72 +12,77 @@
 
 <body>
 
-    <div class="grid grid-cols-3">
-        <label class="font-semibold uppercase" for="">Nombre:</label>
-        <label class="uppercase col-span-2" for="">{{$userAppointment->appointmentUser->name.' '.$userAppointment->appointmentUser->apParental.' '.$userAppointment->appointmentUser->apMaternal}}</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">CURP:</label>
-        <label class="uppercase col-span-2" for="">{{$userAppointment->appointmentUser->curp}}</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Número de expediente:</label>
-        <label class="uppercase col-span-2" for="">1330926</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Modo de transporte:</label>
-        <label class="uppercase col-span-2" for="">Aéreo</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Categoría:</label>
-        <label class="uppercase col-span-2" for="">Grupo I</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Puesto:</label>
-        <label class="uppercase col-span-2" for="">Piloto transp. púb. ilimitado de ala fija o </label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Trámite:</label>
-        <label class="uppercase col-span-2" for="">Examen Psicofísico integral</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">No de cita:</label>
-        <label class="uppercase col-span-2" for="">9965278</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Unidad médica:</label>
-        <label class="uppercase col-span-2" for="">Ciudad de México UM aeropuerto</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Mes:</label>
-        <label class="uppercase col-span-2" for="">Octubre</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Día:</label>
-        <label class="uppercase col-span-2" for="">Jueves 20</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase" for="">Hora:</label>
-        <label class="uppercase col-span-2" for="">8:00</label>
-    </div>
-    <div class="grid gap-x-8 grid-cols-3">
-        <label class="font-semibold uppercase space-y-12" for="">Folio:</label>
-        <label class="uppercase col-span-2" for="">996527829/10/2022psb8:00</label>
-    </div>
-
-    <div class="grid-cols-2">
-        <label class="font-semibold uppercase mt-36 inline-flex" for="">Requisitos:</label>
-        <label class="" for="">Si alguno de estos documentos no son presentados el día
-            de su cita, no podrá realizar su examen por lo que este se perderá. Identificación oficial (Se acepta
-            únicamente INE vigente, Cédula de identidad ciudadana, Cédula profesional, cartilla militar, licencia
-            federal, título, certificado o libreta de mar y de identificación marítima) ORIGINAL Y COPIA.
-            Comprobante de domicilio (con vigencia no mayor a 3 meses)COPIA. Comprobante de pago. ORIGINAL. CURP
-            COPIA.
-        </label>
-    </div>
-    <div class="mt-4 relative flex py-5 items-center">
-        <h1 class="text-left after:align-bottom after:w-72 after:bg-black after:h-[1px] after:inline-block after:relative after:left-2"
-            for="">Firma</h1>
+<div>
+         {{--<img src="{{ asset('images/headerOrigen.jpg') }}" align="center" style="padding-top:1%;margin-left:1%;" width="300" height="110" alt="">--}}
+         <div class="titulo">
+             <h3>COMPROBANTE DE CITA</h3>
+         </div>
+        
+        <table>
+            <tr>
+                <td colspan="12">NOMBRE</td>
+                <td colspan="24">{{$userAppointment->appointmentUser->name.' '.$userAppointment->appointmentUser->apParental.' '.$userAppointment->appointmentUser->apMaternal}}</td>
+            </tr>
+            <tr>
+                <td colspan="12">CURP:</td>
+                <td>{{$userAppointment->appointmentUser->curp}}</td>
+            </tr>
+            <tr>
+                <td colspan="12">NUMERO DE EXPEDIENTE:</td>
+                <td colspan="24">1330926</td>
+            </tr>
+            <tr>
+                <td colspan="12">MODO DE TRASPORTE:</td>
+                <td colspan="24">AÉREO</td>
+            </tr>
+             <tr>
+                <td colspan="12">TIPO DE EXAMEN:</td>
+                <td colspan="24">TIPO DE EXAMEN</td>
+            </tr>
+            <tr>
+                <td colspan="12">TIPO DE CLASE</td>
+                <td colspan="24">TIPO DE CLASE</td>
+            </tr>
+            <tr>
+                <td colspan="12">TRAMITE</td>
+                <td colspan="24">EXAMEN PSICOFISICO INTEGRAL</td>
+            </tr>
+            <tr>
+                <td colspan="12">NO. DE CITA</td>
+                <td colspan="24">9965278</td>
+            </tr>
+            <tr>
+                <td colspan="12">UNIDAD MÉDICA:</td>
+                <td colspan="24">Ciudad de México UM aeropuerto</td>
+            </tr>
+            <tr>
+                <td colspan="12">MES:</td>
+                <td colspan="24">Octubre</td>
+            </tr>
+             <tr>
+                <td colspan="12">DIA:</td>
+                <td colspan="24">Jueves 20</td>
+            </tr>
+             <tr>
+                <td colspan="12">HORA:</td>
+                <td colspan="24">8:00</td>
+            </tr>
+            <tr>
+                <td colspan="12">FOLIO:</td>
+                <td colspan="24">EXAMEN PSICOFISICO INTEGRAL</td>
+            </tr>
+        </table>
+        <div style="padding-top:5%">
+            <label  for="">Requisitos:</label>
+            <div style="padding-left:18%;padding-top:-3%;text-align: justify;"><label >Si alguno de estos documentos no son presentados el día de su cita, no podrá realizar su examen
+            por lo que este se perderá. Identificación oficial (Se acepta únicamente INE vigente, Cédula de identidad
+            ciudadana, Cédula profesional, cartilla militar, licencia federal, título, certificado o libreta de mar y de
+            identificación marítima) ORIGINAL Y COPIA. Comprobante de domicilio (con vigencia no mayor a 3
+            meses)COPIA. Comprobante de pago. ORIGINAL. CURP COPIA. </label></div>
+            
+        </div>
+        <p style="padding-left:15%">FIRMA: </p>
+        <hr Style="margin-top:-2%" width="50%">
     </div>
 </body>
 
