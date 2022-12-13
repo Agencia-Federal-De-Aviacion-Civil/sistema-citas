ee67cd52658a2223b0c0fc868e70aa79a6c97844
Alberto-AFAC <jmondragonescamilla@gmail.com>
2022-12-13 13:25:34 -0600

    se modifica la ruta de acceso para que pueda funcionar

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    // Route::get('/', [homeController::class, 'index'])->name('afac.home');
+    Route::get('/', [homeController::class, 'index'])->name('afac.login');
 });
