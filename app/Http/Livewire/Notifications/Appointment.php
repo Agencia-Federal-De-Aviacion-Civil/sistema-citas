d301782d4dff14962889926b5630a6a0567e3839
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-02-09 16:17:03 -0600

    se crea la tabla para notifications

---
+++ app/Http/Livewire/Notifications/Appointment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Notifications;
+
+use Livewire\Component;
+
+class Appointment extends Component
+{
+    public function render()
+    {
+        return view('livewire.notifications.appointment');
+    }
+}
