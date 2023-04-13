15320834d44f05eecec64e0ed9d42a20a9bb26d4
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-04-13 17:03:58 -0600

    se arregla input

--- resources/views/livewire/register/index.blade.php
+++ resources/views/livewire/register/index.blade.php
@@ -102,8 +102,10 @@
                                             class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900">{{ $message }}</span>
                                     @enderror --}}
                                     <div class="relative mb-6 w-full group">
-                                        <x-inputs.maskable class="uppercase" wire:model.defer="curp"
-                                            mask="AAAA######AAAAAA##" label="CURP" placeholder="INGRESE..." />
+                                        {{-- <x-inputs.maskable class="uppercase" wire:model.defer="curp"
+                                            mask="AAAA######AAAAAA##" label="CURP" placeholder="INGRESE..." /> --}}
+                                            <x-input class="uppercase" wire:model.defer="curp"
+                                            label="CURP" placeholder="INGRESE..." />
                                         {{-- <input type="text" name="floating_curp" id="floating_curp"
                                             wire:model.lazy="curp"
                                             class="uppercase block py-2.5 px-0 w-full text-regular text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
