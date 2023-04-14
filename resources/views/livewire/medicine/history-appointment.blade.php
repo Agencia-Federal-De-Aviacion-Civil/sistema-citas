c87fdbd5f6a372861ed4df839ab97358a3aae89d
jessicasoto01afac <jessica.soto928@gmail.com>
2023-04-14 12:08:27 -0600

    historial

--- resources/views/livewire/medicine/history-appointment.blade.php
+++ resources/views/livewire/medicine/history-appointment.blade.php
@@ -10,7 +10,8 @@
         <div
             class="z-10 relative container px-6 mx-auto flex flex-col md:flex-row items-start md:items-center justify-between">
             <div>
-                <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">Citas agendadas</h4>
+                <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">CITAS AGENDADAS
+                </h4>
                 <ul class="flex flex-col md:flex-row items-start md:items-center text-gray-300 text-sm mt-3">
                     <li class="flex items-center mt-4 md:mt-0">
                     </li>
@@ -53,8 +54,7 @@
                             </tr>
                         </thead>
                         <tbody>
-
-                            @foreach ($medicineInitial as $medicineQuerie)
+                            @foreach ($medicineReserves as $medicineReserve)
                                 <tr
                                     class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <th scope="row"
@@ -63,103 +63,45 @@
                                     </th>
 
                                     <td class="px-6 py-4">
-                                        {{ $medicineQuerie->initialMedicine->medicineUser->name }}
+                                        {{ $medicineReserve->medicineReserveMedicine->medicineUser->name }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        {{ $medicineQuerie->initialMedicine->medicineTypeExam->name }}
+                                        {{ $medicineReserve->medicineReserveMedicine->medicineTypeExam->name }}
                                     </td>
 
-                                    @if ($medicineQuerie->medicineInitialTypeClass->type_exam_id == 1)
+                                    @if ($medicineReserve->medicineReserveMedicine->medicineTypeExam->id == 1)
                                         <td class="px-6 py-4">
-                                            {{ $medicineQuerie->medicineInitialTypeClass->name }}
+                                            {{ $medicineReserve->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
                                         </td>
                                         <td class="px-6 py-4">
-                                            {{ $medicineQuerie->medicineInitialClasificationClass->name }}
+                                            {{ $medicineReserve->medicineReserveMedicine->medicineInitial[0]->medicineInitialClasificationClass->name }}
+                                        </td>
+                                    @elseif($medicineReserve->medicineReserveMedicine->medicineTypeExam->id == 2)
+                                        <td class="px-6 py-4">
+                                            {{ $medicineReserve->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $medicineReserve->medicineReserveMedicine->medicineRenovation[0]->renovationClasificationClass->name }}
                                         </td>
                                     @endif
                                     <td class="px-6 py-4">
-                                        {{ $medicineQuerie->initialMedicine->medicineReserve[0]->user->name }}
+                                        {{ $medicineReserve->user->name }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        {{$medicineQuerie->initialMedicine->medicineReserve[0]->dateReserve}}
-                                        {{-- {{ $appointment->appointmentSuccess->appointmentDate . ' ' . $appointment->appointmentSuccess->appointmentTime }} --}}
+                                        {{ $medicineReserve->dateReserve }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        {{-- <x-button wire:click="rescheduleAppointment({{ $appointment->appointmentSuccess->id }})" label="REAGENDAR" xs blue right-icon="calendar" /> --}}
+                                        {{-- <x-button wire:click="rescheduleAppointment({{ $medicineReserve->from_user_appointment  }})" label="REAGENDAR" xs blue right-icon="calendar" /> --}}
                                         {{-- <x-button wire:click="rescheduleAppointment()" label="REAGENDAR" xs blue right-icon="calendar" />                                             --}}
                                     </td>
                                     <td class="px-6 py-4">
-                                        {{-- <x-button wire:click="deletAppointment({{ $appointment->appointmentSuccess->id }})" label="ELIMINAR" xs red right-icon="trash" /> --}}
-                                       {{-- <x-button wire:click="deletAppointment()" label="ELIMINAR" xs red right-icon="trash" /> --}}
+                                        {{-- <x-button wire:click="deletAppointment({{ $medicineReserve->from_user_appointment  }})" label="ELIMINAR" xs red right-icon="trash" /> --}}
+                                        {{-- <x-button wire:click="deletAppointment()" label="ELIMINAR" xs red right-icon="trash" /> --}}
 
                                     </td>
                                 </tr>
                         </tbody>
                         @endforeach
-
-
-
-
-                        @foreach ($medicineReserves as $medicineReserve)
-                        <tr
-                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row"
-                                class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                {{ $n++ }}
-                            </th>
-
-                            <td class="px-6 py-4">
-                                {{ $medicineReserve->renovationMedicine->medicineUser->name }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $medicineReserve->renovationMedicine->medicineTypeExam->name }}
-                            </td>
-
-                            @if($medicineReserve->renovationTypeClass->type_exam_id == 2)
-                                <td class="px-6 py-4">
-                                    {{ $medicineReserve->renovationTypeClass->name }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $medicineReserve->renovationClasificationClass->name }}
-                                </td>
-                            @endif
-                            <td class="px-6 py-4">
-                                {{ $medicineReserve->renovationMedicine->medicineReserve[0]->user->name }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $medicineReserve->renovationMedicine->medicineReserve[0]->dateReserve}}
-                                {{-- {{ $appointment->appointmentSuccess->appointmentDate . ' ' . $appointment->appointmentSuccess->appointmentTime }} --}}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{-- <x-button wire:click="rescheduleAppointment({{ $appointment->appointmentSuccess->id }})" label="REAGENDAR" xs blue right-icon="calendar" /> --}}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{-- <x-button wire:click="deletAppointment({{ $appointment->appointmentSuccess->id }})" label="ELIMINAR" xs red right-icon="trash" /> --}}
-                            </td>
-                        </tr>
-                </tbody>
-                @endforeach
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                     </table>
                     <div class="mt-6 ml-6 mr-6 mb-6">
                     </div>
