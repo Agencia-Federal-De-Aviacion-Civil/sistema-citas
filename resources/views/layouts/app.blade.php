8064542a091ffee3967039b4d442526632ac616b
jessicasoto01afac <jessica.soto928@gmail.com>
2023-04-18 16:58:46 -0600

    dasboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <script src="https://cdn.jsdelivr.net/npm/countup@1.8.2/dist/countUp.min.js"></script>
 
     <!-- Scripts -->
     @wireUiScripts
