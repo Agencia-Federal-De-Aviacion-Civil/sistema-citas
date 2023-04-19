9e8e4e649d26534366ba3c7e7b6ccf5455994a76
jessicasoto01afac <jessica.soto928@gmail.com>
2023-04-19 16:20:57 -0600

    il

---
+++ app/View/Components/headquarts-component.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class headquartsComponent extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $scheduleId,$status;
+    public $modal;
+    public function __construct($scheduleId,$status)
+    {
+        $this->scheduleId = $scheduleId;
+        $this->status = $status;   
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    
+
+    public function render()
+    {
+        return view('components.schedule-component');
+    }
+}
