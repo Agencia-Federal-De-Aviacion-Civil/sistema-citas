639135d079292bb426f26d8023c8ee43f604a429
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-04-24 10:25:00 -0600

    se crea la tabla para sedes

---
+++ app/Http/Livewire/HeadquarterTable.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Catalogue\Headquarter;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Rules\{Rule, RuleActions};
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+use PowerComponents\LivewirePowerGrid\{Button, Column, Exportable, Footer, Header, PowerGrid, PowerGridComponent, PowerGridEloquent};
+
+final class HeadquarterTable extends PowerGridComponent
+{
+    use ActionButton;
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Features Setup
+    |--------------------------------------------------------------------------
+    | Setup Table's general features
+    |
+    */
+    public function setUp(): array
+    {
+        // $this->showCheckBox();
+
+        return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+
+    /**
+     * PowerGrid datasource.
+     *
+     * @return Builder<\App\Models\Catalogue\Headquarter>
+     */
+    public function datasource(): Builder
+    {
+        return Headquarter::query()->with(['headquarterUser']);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+
+    /**
+     * Relationship search.
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    | ❗ IMPORTANT: When using closures, you must escape any value coming from
+    |    the database using the `e()` Laravel Helper function.
+    |
+    */
+    public function addColumns(): PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            ->addColumn('id')
+            ->addColumn('name', function (Headquarter $user) {
+                return $user->headquarterUser->name;
+            })
+            ->addColumn('email', function (Headquarter $user) {
+                return $user->headquarterUser->email;
+            })
+            ->addColumn('direction')
+            ->addColumn('url', function (Headquarter $url) {
+                return '<a href=' . e($url->url) . '>' . e($url->url) . '</a>';
+            });
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+    */
+
+    /**
+     * PowerGrid Columns.
+     *
+     * @return array<int, Column>
+     */
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'id'),
+            Column::make('SEDE', 'name')
+                ->searchable()
+                ->sortable(),
+            Column::make('CORREO', 'email'),
+            Column::make('DIRECCIÓN', 'direction'),
+            Column::make('URL', 'url')
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable the method below only if the Routes below are defined in your app.
+    |
+    */
+
+    /**
+     * PowerGrid Headquarter Action Buttons.
+     *
+     * @return array<int, Button>
+     */
+
+    /*
+    public function actions(): array
+    {
+       return [
+           Button::make('edit', 'Edit')
+               ->class('bg-indigo-500 cursor-pointer text-white px-3 py-2.5 m-1 rounded text-sm')
+               ->route('headquarter.edit', ['headquarter' => 'id']),
+
+           Button::make('destroy', 'Delete')
+               ->class('bg-red-500 cursor-pointer text-white px-3 py-2 m-1 rounded text-sm')
+               ->route('headquarter.destroy', ['headquarter' => 'id'])
+               ->method('delete')
+        ];
+    }
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Rules
+    |--------------------------------------------------------------------------
+    | Enable the method below to configure Rules for your Table and Action Buttons.
+    |
+    */
+
+    /**
+     * PowerGrid Headquarter Action Rules.
+     *
+     * @return array<int, RuleActions>
+     */
+
+    /*
+    public function actionRules(): array
+    {
+       return [
+
+           //Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($headquarter) => $headquarter->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
--- resources/views/livewire/appointment/headquarters/headquarters.blade.php
+++ resources/views/livewire/appointment/headquarters/headquarters.blade.php
@@ -29,93 +29,33 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="px-6 bg-white shadow-xl sm:rounded-lg">
-                <div class="mt-5 mb-4">
-                    <div class="relative overflow-x-auto sm:rounded-lg">
-                        <div class="p-4">
-                            <x-button wire:click.prevent="addHeadquarter()" right-icon="view-grid-add" blue
-                                label="AÑADIR SEDE" />
-                            <label for="table-search" class="sr-only">Search</label>
-                            <div class="my-6 relative mt-1 float-right">
-                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                            clip-rule="evenodd"></path>
-                                    </svg>
-                                </div>
-                                <input type="text" wire:model="search"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Buscar...">
+                <div class="ml-4 py-6 mr-4 uppercase text-sm">
+                    <div x-data="{ activeTab: 'headquarter' }">
+                        <ul
+                            class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 ">
+                            <li class="mr-2">
+                                <a href="#" class="inline-block p-4 text-gray-800 bg-white rounded-t-lg active"
+                                    x-on:click.prevent="activeTab = 'headquarter'"
+                                    :class="{ 'active': activeTab === 'headquarter' }">
+                                    SEDES
+                                </a>
+                            </li>
+                            <li class="mr-2">
+                                <a href="#"
+                                    class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50"
+                                    x-on:click.prevent="activeTab = 'schedule'"
+                                    :class="{ 'active': activeTab === 'schedule' }">
+                                    HORARIOS
+                                </a>
+                            </li>
+                        </ul>
+                        <div x-show="activeTab === 'headquarter'">
+                            <div class="mt-8">
+                                <livewire:headquarter-table />
                             </div>
                         </div>
-                        <div class="ml-4 py-6 mr-4">
-                            {{-- @if ($usuarios->count()) --}}
-                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                <thead
-                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                    <tr>
-                                        <th scope="col" class="px-6 py-3">
-                                            ID
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            NOMBRE COMPLETO
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            CORREO
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            DIRECCIÓN
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            URL
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($headquarters as $headquarter)
-                                        <tr
-                                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                            <th scope="row"
-                                                class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-
-                                            </th>
-                                            <td class="px-6 py-4">
-                                                {{ $headquarter->headquarterUser->name }}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {{ $headquarter->headquarterUser->email }}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {{ $headquarter->direction }}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                <a target="_blank"
-                                                    class="text-blue-600 dark:text-blue-500 hover:underline"
-                                                    href="{{ $headquarter->url }}">
-                                                    {{ $headquarter->url }}</a>
-                                            </td>
-                                            <td class="px-6 py-4">
-
-                                                <button wire:click="editModal({{ $headquarter->id }})"
-                                                    class="px-3 py-2 text-xs font-medium text-center text-white bg-sky-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                                    Editar
-                                                </button>
-                                                <button wire:click=""
-                                                    class="px-3 py-2 text-xs font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-blue-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                                                    Eliminar
-                                                </button>
-
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            <div class="mt-6 ml-6 mr-6 mb-6">
-                            </div>
+                        <div x-show="activeTab === 'schedule'">
+                            OTRO
                         </div>
                     </div>
                 </div>
