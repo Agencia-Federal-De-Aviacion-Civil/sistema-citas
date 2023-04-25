2f4a78948f04336079dcf990775038270afac013
jessicasoto01afac <jessica.soto928@gmail.com>
2023-04-25 16:49:41 -0600

    arreglos de nombres

--- resources/views/livewire/medicine/modals/schedule.blade.php
+++ resources/views/livewire/medicine/modals/schedule.blade.php
@@ -10,7 +10,7 @@
                     @if ($this->status == 0)
                     CITA
                 @elseif ($this->status == 1)
-                    CITA ASISTIDA
+                    CITA VALIDA
                 @elseif ($this->status == 2)
                     CITA CANCELADA
                 @elseif ($this->status == 3)
