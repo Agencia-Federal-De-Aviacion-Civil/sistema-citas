4819703ed6b37f71064c9dac4b85d0fbcb123643
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-05-02 09:52:39 -0600

    aun falta detallar, no funcionan las tareas en windows 11

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,15 +7,13 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
+
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('cancel:appointments')->everyMinute();
+        //   $schedule->call(function () {
+        //     info('PRUEBA DE SCHEDULE');
+        // })->everyMinute()->timezone('America/Mexico_City');
     }
 
     /**
@@ -25,7 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
