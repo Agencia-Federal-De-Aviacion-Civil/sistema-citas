bfd3350d3bbdbb43ddbef6977ac9e07e5fb36b59
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-05-02 17:56:34 -0600

    se guardan los roles y permisos

--- resources/views/afac/admin/roles/home-role.blade.php
+++ resources/views/afac/admin/roles/home-role.blade.php
@@ -6,7 +6,7 @@
             class="z-10 relative container px-6 mx-auto flex flex-col md:flex-row items-start md:items-center justify-between">
             <div>
                 <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">Administración
-                    de Perfiles y Roles</h4>
+                    de Roles y Permisos</h4>
                 <ul class="flex flex-col md:flex-row items-start md:items-center text-gray-300 text-sm mt-3">
                     <li class="flex items-center mt-4 md:mt-0">
                         <div class="mr-1">
@@ -24,12 +24,34 @@
     <div class="py-12">
         <div class="container mx-auto px-4 py-4 bg-white shadow-xl sm:rounded-lg">
             <div class="mt-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
-                @livewire('permissions.index')
-                {{-- @livewire('roles.index') --}}
-                <a type="button" href="{{ route('afac.roles.create') }}"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Añadir
-                    nuevo</a>
-                <livewire:role-table />
+                <div x-data="{ activeTab: 'roles' }">
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 ">
+                        <li class="mr-2">
+                            <a href="#" class="inline-block p-4 text-gray-800 bg-white rounded-t-lg active"
+                                x-on:click.prevent="activeTab = 'roles'" :class="{ 'active': activeTab === 'roles' }">
+                                ROLES
+                            </a>
+                        </li>
+                        <li class="mr-2">
+                            <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50"
+                                x-on:click.prevent="activeTab = 'permissions'"
+                                :class="{ 'active': activeTab === 'permissions' }">
+                                PERMISOS
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="mt-6">
+                        <div x-show="activeTab === 'roles'">
+                            <a type="button" href="{{ route('afac.roles.create') }}"
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Añadir
+                                nuevo</a>
+                            <livewire:role-table />
+                        </div>
+                        <div x-show="activeTab === 'permissions'">
+                            @livewire('permissions.index')
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/permissions/index.blade.php
+++ resources/views/livewire/permissions/index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-notifications position="top-bottom" />
     <div class="mb-6">
-        <x-button wire:click="$emit('openModal', 'permissions.modals.create')" icon="pencil" primary
+        <x-button wire:click="$emit('openModal', 'permissions.modals.create-update-modal')" icon="pencil" primary
             label="AÑADIR" />
     </div>
     <livewire:permission-table />
