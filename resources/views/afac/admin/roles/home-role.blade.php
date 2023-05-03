eff346353139438146eb47fc0303ac72ad9e19bd
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-05-03 10:24:05 -0600

    se arregla el boton de roles

--- resources/views/afac/admin/roles/home-role.blade.php
+++ resources/views/afac/admin/roles/home-role.blade.php
@@ -43,8 +43,8 @@
                     <div class="mt-6">
                         <div x-show="activeTab === 'roles'">
                             <a type="button" href="{{ route('afac.roles.create') }}"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Añadir
-                                nuevo</a>
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">AÑADIR
+                            </a>
                             <livewire:role-table />
                         </div>
                         <div x-show="activeTab === 'permissions'">
