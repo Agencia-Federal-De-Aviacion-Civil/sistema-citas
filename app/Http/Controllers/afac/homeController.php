cd823d7a83ee8f865ec583ecc91d8afdf1f6c81e
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-05-08 16:25:31 -0600

    aqui se manda la prueba del dashboard

--- app/Http/Controllers/afac/homeController.php
+++ app/Http/Controllers/afac/homeController.php
@@ -19,42 +19,58 @@ class homeController extends Controller
         $date = Date::now()->parse();
         $date1 = Date::now()->format('Y-m-d');
         $appointment = MedicineReserve::query()
-            ->selectRaw("count(id) as registradas")
-            ->selectRaw("count(case when status = '0' then 1 end) as pendientes")
-            ->selectRaw("count(case when status = '1' then 1 end) as validado")
-            ->selectRaw("count(case when status = '2' then 1 end) as canceladosede")
-            ->selectRaw("count(case when status = '3' then 1 end) as canceladousuario")
-            ->selectRaw("count(case when status = '4' then 1 end) as reagendado")
-            ->selectRaw("count(case when dateReserve = ? then 1 end) as appointmentnow", [$date1])
-            ->selectRaw("count(id) as registradasfull")
-            // ->where('dateReserve', $date1)
-            ->first();
-        //ADMIN MEDICINE
-        $registradas = $appointment->registradas;
-        $pendientes = $appointment->pendientes;
-        $validado = $appointment->validado;
-        $reagendado = $appointment->reagendado;
-        $canceladas = $appointment->canceladosede + $appointment->canceladousuario;
-        $porconfir = $appointment && $appointment->registradas > 0
-            ? round(($appointment->validado * 100 / $appointment->registradas), 0)
-            : 0;
-        $porpendientes = $appointment && $appointment->registradas > 0
-            ? round(($appointment->pendientes * 100 / $appointment->registradas), 0)
-            : 0;
-        $porreagendado = $appointment && $appointment->registradas > 0
-            ? round(($appointment->reagendado * 100 / $appointment->registradas), 0)
-            : 0;
-        $porcanceladas1 = $appointment && $appointment->registradas > 0
-            ? round(($appointment->canceladas * 100 / $appointment->registradas), 0)
-            : 0;
-        $porcanceladas = $appointment && $appointment->registradas > 0
-            ? round(($canceladas * 100 / $registradas), 0)
-            : 0;
-        //dd($canceladas* 100 / $registradas);
-        $nowapoimnet = $appointment->appointmentnow;
-        //SUPER ADMIN
-        $registradasall = $appointment->registradasfull;
-        $medicine = ($registradasall ? $registradasall * 100 / $registradasall : '0');
+            ->select('status', DB::raw('count(*) as count'), DB::raw('dateReserve'))
+            ->where('dateReserve', $date1)
+            ->groupBy('status', 'dateReserve')
+            ->get();
+        $appointmentNow = $appointment->where('dateReserve', $date1);
+        $now = $appointmentNow->sum('count');
+        $registradas = $appointment->sum('count');
+        $porconfir = round($appointment->where('status', '1')->sum('count') * 100 / $registradas, 0);
+        $validado = $appointment->where('status', '1')->sum('count');
+        $pendientes = $appointment->where('status', '0')->sum('count');
+        $porpendientes = round($appointment->where('status', '0')->sum('count') * 100 / $registradas, 0);
+        $canceladas = $appointment->whereIn('status', ['2', '3'])->sum('count');
+        $reagendado = $appointment->where('status', '4')->sum('count');
+        $porreagendado = $appointment->where('status', '4')->sum('count') * 100 / $registradas;
+        $porcanceladas = round($appointment->whereIn('status', ['2', '3'])->sum('count') * 100 / $registradas,0);
+        // $appointment = MedicineReserve::query()
+        //     ->selectRaw("count(id) as registradas")
+        //     ->selectRaw("count(case when status = '0' then 1 end) as pendientes")
+        //     ->selectRaw("count(case when status = '1' then 1 end) as validado")
+        //     ->selectRaw("count(case when status = '2' then 1 end) as canceladosede")
+        //     ->selectRaw("count(case when status = '3' then 1 end) as canceladousuario")
+        //     ->selectRaw("count(case when status = '4' then 1 end) as reagendado")
+        //     ->selectRaw("count(case when dateReserve = ? then 1 end) as appointmentnow", [$date1])
+        //     ->selectRaw("count(id) as registradasfull")
+        //     // ->where('dateReserve', $date1)
+        //     ->first();
+        // //ADMIN MEDICINE
+        // $registradas = $appointment->registradas;
+        // $pendientes = $appointment->pendientes;
+        // $validado = $appointment->validado;
+        // $reagendado = $appointment->reagendado;
+        // $canceladas = $appointment->canceladosede + $appointment->canceladousuario;
+        // $porconfir = $appointment && $appointment->registradas > 0
+        //     ? round(($appointment->validado * 100 / $appointment->registradas), 0)
+        //     : 0;
+        // $porpendientes = $appointment && $appointment->registradas > 0
+        //     ? round(($appointment->pendientes * 100 / $appointment->registradas), 0)
+        //     : 0;
+        // $porreagendado = $appointment && $appointment->registradas > 0
+        //     ? round(($appointment->reagendado * 100 / $appointment->registradas), 0)
+        //     : 0;
+        // $porcanceladas1 = $appointment && $appointment->registradas > 0
+        //     ? round(($appointment->canceladas * 100 / $appointment->registradas), 0)
+        //     : 0;
+        // $porcanceladas = $appointment && $appointment->registradas > 0
+        //     ? round(($canceladas * 100 / $registradas), 0)
+        //     : 0;
+        // //dd($canceladas* 100 / $registradas);
+        // $nowapoimnet = $appointment->appointmentnow;
+        // //SUPER ADMIN
+        // $registradasall = $appointment->registradasfull;
+        // $medicine = ($registradasall ? $registradasall * 100 / $registradasall : '0');
 
 
 
@@ -64,6 +80,6 @@ class homeController extends Controller
         $headquarters = Headquarter::with([
             'headquarterUser'
         ])->get();
-        return view('afac.dashboard.index', compact('date', 'registradasall', 'medicine', 'registradas', 'pendientes', 'validado', 'reagendado', 'canceladas', 'porconfir', 'porpendientes', 'porreagendado', 'porcanceladas1', 'porcanceladas', 'date1', 'headquarters','nowapoimnet'));
+        return view('afac.dashboard.index', compact('headquarters', 'registradas', 'pendientes', 'validado', 'canceladas', 'reagendado', 'porconfir', 'porpendientes', 'porreagendado', 'porcanceladas', 'now', 'date'));
     }
 }
--- resources/views/afac/dashboard/dashboard_superadmin.blade.php
+++ resources/views/afac/dashboard/dashboard_superadmin.blade.php
@@ -21,7 +21,7 @@
     <div class="py-12">
         <div class="container mx-auto px-4 py-4 bg-white shadow-xl sm:rounded-lg">
             <div class="bg-gray-100 text-gray-500 rounded shadow-xl py-5 px-4 w-full sm:w-full md:w-full lg:w-full">
-                <h2 class="mb-4 text-2xl font-bold">TOTAL DE CITAS {{ $registradasall }}</h2>
+                <h2 class="mb-4 text-2xl font-bold">TOTAL DE CITAS {{ $registradas }}</h2>
                 <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
                     <!-- 1 card -->
                     <div class="relative bg-white py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
@@ -43,13 +43,13 @@
                                 <div class="my-2">
                                     <p class="font-semibold text-base mb-2">Total de citas</p>
                                     <div class="flex space-x-2">
-                                        <p>{{ $registradasall }}</p>
+                                        <p>{{ $registradas }}</p>
                                     </div>
                                 </div>
                                 <div class="my-2">
                                     <p class="font-semibold text-base mb-2">Progress</p>
                                     <div class="text-base text-gray-400 font-semibold">
-                                        <p>{{ $medicine }}%</p>
+                                        <p>{{ $registradas }}%</p>
                                     </div>
                                 </div>
                             </div>
@@ -209,7 +209,7 @@
 
                                 <div class="ml-4">
                                     <h2 class="font-semibold">Canceladas: {{ $canceladas }}</h2>
-                                    <p class="mt-2 text-sm text-gray-500">{{ $porcanceladas }}%</p>
+                                    <p class="mt-2 text-sm text-gray-500">{{ $porcanceladas }}%</p>    
                                 </div>
                             </div>
                         </div>
@@ -303,7 +303,7 @@
                                         </svg>
                                     </div>
                                     <div class="ml-4">
-                                        <h2 class="font-semibold">{{ $nowapoimnet }} Citas</h2>
+                                        <h2 class="font-semibold">{{ $now }} Citas</h2>
                                         <p class="mt-2 text-sm text-gray-500">hoy {{ $date }}</p>
                                     </div>
                                 </div>
@@ -336,7 +336,7 @@
                     "color": "blue-500"
                 }, {
                     "label": "Cancelado",
-                    "size": @json($porcanceladas),
+                    "size": @json($porcanceladas)
                     "color": "red-500"
                 }, ]
             }
