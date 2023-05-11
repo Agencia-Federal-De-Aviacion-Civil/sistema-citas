d6c2450fdc82d7d218a9b99a5fe347f36e310f56
jessicasoto01afac <jessica.soto928@gmail.com>
2023-05-11 17:56:41 -0600

    Dash de medicos

--- resources/views/afac/dashboard/dashboard_medicine.blade.php
+++ resources/views/afac/dashboard/dashboard_medicine.blade.php
@@ -102,9 +102,19 @@
                             <thead>
                                 <tr>
                                     <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        Nombre</th>
-                                    <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                                        total de citas</th>
+                                                Sede</th>
+                                            <th
+                                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                                Citas para hoy
+                                            </th>
+                                            <th
+                                              class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                               Citas para mañana
+                                            </th>
+                                            <th
+                                                class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                                total de citas
+                                            </th>
                                     <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">
                                     </th>
                                 </tr>
@@ -112,32 +122,46 @@
                             {{-- {{$headquarters}} --}}
                             <tbody>
                                 @foreach ($headquarters as $headquarter)
-                                <tr class="text-gray-700 dark:text-gray-100">
-                                    <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                                        {{ $headquarter->headquarterUser->name }}</th>
-                                    <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                        {{ $headquarter->headquarterUser->userMedicineReserveTo->count() }}
-                                    </td>
-                                    <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                                        <div class="flex items-center">
-                                            <span class="mr-2">{{ round(($headquarter->headquarterUser->userMedicineReserveTo->count() * 100) / $registradas, 1) }}%</span>
-                                            <div class="relative w-full">
-                                                <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                                                    <div style="width:{{ ($headquarter->headquarterUser->userMedicineReserveTo->count() * 100) / $registradas }}%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600">
+                                            <tr class="text-gray-700 dark:text-gray-100">
+                                                <th
+                                                    class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                                                    {{ $headquarter->headquarterUser->name }}</th>
+                                                <td
+                                                    class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                                    {{ $headquarter->headquarterUser->userMedicineReserveTo->where('dateReserve', $date1)->whereIn('status',['0','1','4'])->count() }}
+                                                </td>
+                                                <td
+                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                                {{ $headquarter->headquarterUser->userMedicineReserveTo->where('dateReserve', $tomorrow)->whereIn('status',['0','1','4'])->count() }}
+                                            </td>
+                                                <td
+                                                class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                                {{ $headquarter->headquarterUser->userMedicineReserveTo->count() }}
+                                                </td>
+                                                <td
+                                                    class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                                    <div class="flex items-center">
+                                                        <span
+                                                            class="mr-2">{{ round(($headquarter->headquarterUser->userMedicineReserveTo->count() * 100) / $registradas, 1) }}%</span>
+                                                        <div class="relative w-full">
+                                                            <div
+                                                                class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
+                                                                <div style="width:{{ ($headquarter->headquarterUser->userMedicineReserveTo->count() * 100) / $registradas }}%"
+                                                                    class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600">
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                @endforeach
+                                                </td>
+                                            </tr>
+                                        @endforeach
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
             <!-- Recent Activities -->
-            <div class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+            {{-- <div class="relative flex flex-col min-w-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
                 <div class="rounded-t mb-0 px-0 border-0">
                     <div class="flex flex-wrap items-center px-4 py-2">
                         <div class="relative w-full max-w-full flex-grow flex-1">
@@ -164,7 +188,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
             <!-- ./Recent Activities -->
         </div>
     </div>
