9a619b792a04ddcf3ed16c98e06c6c0e7ad04100
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-05-11 10:08:58 -0600

    se eliminan los componentes

--- resources/views/components/delete-schedule.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <x-button
-    wire:click="$emit('openModal', 'headquarters.modals.create-update-schedule-modal',{{ json_encode(['deleteId' => $deleteId]) }})"
-    xs icon="calendar" info label="EDITAR DIAS" />
-</div>
