6aef95200b48ad7bc61212ad4b1a84b1686f20e6
jessicasoto01afac <jessica.soto928@gmail.com>
2023-05-11 17:57:33 -0600

    banner dash

--- resources/views/afac/dashboard/dashboard_medicine.blade.php
+++ resources/views/afac/dashboard/dashboard_medicine.blade.php
@@ -94,7 +94,21 @@
                 <div class="rounded-t mb-0 px-0 border-0">
                     <div class="flex flex-wrap items-center px-4 py-2">
                         <div class="relative w-full max-w-full flex-grow flex-1">
-                            <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Sedes</h3>
+                            {{-- <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Sedes</h3> --}}
+                            <div class="flex items-start rounded-xl p-4">
+                                <div
+                                    class="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                    </svg>
+                                </div>
+                                <div class="ml-4">
+                                    <h2 class="font-semibold">{{ $now }} Citas</h2>
+                                    <p class="mt-2 text-sm text-gray-500">hoy {{ $date2 }}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="block w-full overflow-x-auto">
