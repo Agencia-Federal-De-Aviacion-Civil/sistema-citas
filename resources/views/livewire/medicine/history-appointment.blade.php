7f116aa6e2aa3ddd51f938f3e1758e3be7475fdc
jessicasoto01afac <jessica.soto928@gmail.com>
2023-05-11 16:58:53 -0600

    capitalize

--- resources/views/livewire/medicine/history-appointment.blade.php
+++ resources/views/livewire/medicine/history-appointment.blade.php
@@ -6,7 +6,7 @@
         <img class="z-0 w-full h-full absolute inset-0 object-cover" src="{{ asset('images/banner_testing.jpg') }}" alt="bg" />
         <div class="z-10 relative container px-6 mx-auto flex flex-col md:flex-row items-start md:items-center justify-between">
             <div>
-                <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">CITAS AGENDADAS
+                <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">Citas Agendadas
                 </h4>
                 <ul class="flex flex-col md:flex-row items-start md:items-center text-gray-300 text-sm mt-3">
                     <li class="flex items-center mt-4 md:mt-0">
