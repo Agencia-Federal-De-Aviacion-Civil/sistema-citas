c3c9c23abd8d4a48f92354f2a96da36e640208d5
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-05-22 13:31:26 -0600

    se manda la liberación de pagos

--- resources/views/components/schedule-component.blade.php
+++ resources/views/components/schedule-component.blade.php
@@ -3,7 +3,7 @@
     @if ($status == 0)
         @hasrole('super_admin|medicine_admin')
             <x-button
-                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId]) }})"
+                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId, 'medicineId' => $medicineId]) }})"
                 label="PENDIENTE" xs silver />
         @else
             <x-badge flat info label="PENDIENTE" />
@@ -11,7 +11,7 @@
     @elseif($status == 1)
         @hasrole('super_admin|medicine_admin')
             <x-button
-                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId]) }})"
+                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId, 'medicineId' => $medicineId]) }})"
                 label=" ASISTIÓ " xs green />
         @else
             <x-badge flat green label="ASISTIÓ" />
@@ -19,7 +19,7 @@
     @elseif($status == 2)
         @hasrole('super_admin|medicine_admin')
             <x-button
-                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId]) }})"
+                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId, 'medicineId' => $medicineId]) }})"
                 label="CANCELADO" xs warning />
         @else
             <x-badge flat negative label="CANCELADA" />
@@ -27,19 +27,18 @@
     @elseif($status == 3)
         @hasrole('super_admin|medicine_admin')
             <x-button
-                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId]) }})"
+                wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId, 'medicineId' => $medicineId]) }})"
                 label="USUARIO CANCELÓ" xs warning />
         @else
             <x-badge flat negative label="CANCELADA" />
         @endhasrole
     @elseif($status == 4)
         <x-button
-            wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId]) }})"
+            wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId, 'medicineId' => $medicineId]) }})"
             label="REAGENDADA" xs warning />
         @hasrole('super_admin|medicine_admin|user')
-        <x-button xs positive href="{{ route('afac.downloadFile', $scheduleId) }}" label="DESCARGAR" />
+            <x-button xs positive href="{{ route('afac.downloadFile', $scheduleId) }}" label="DESCARGAR" />
         @endhasrole
-
     @endif
     @if ($status == 0)
         @hasrole('super_admin|medicine_admin|user')
