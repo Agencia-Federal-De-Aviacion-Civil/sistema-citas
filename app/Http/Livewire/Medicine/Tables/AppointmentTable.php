727d4f7a4d31e15ff1290c79547eb772420dc30b
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-05 11:51:46 -0600

    se hacen algunos ajustes para exportar las tablas en excel

---
+++ app/Http/Livewire/Medicine/Tables/AppointmentTable.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Livewire\Medicine\Tables;
+
+use App\Events\Medicine\ExportCompleted;
+use App\Jobs\ExportSelectedJob;
+use App\Models\Medicine\MedicineReserve;
+use Illuminate\Database\Eloquent\Builder;
+use Livewire\Component;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use WireUi\Traits\Actions;
+
+class AppointmentTable extends DataTableComponent
+{
+    use Actions;
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+
+        $this->setBulkActions([
+            'exportSelected' => 'EXPORTAR'
+        ]);
+        $this->setOfflineIndicatorEnabled();
+        $this->setEagerLoadAllRelationsEnabled();
+    }
+
+
+    public function columns(): array
+    {
+        return [
+
+            Column::make("Id", "id")
+                ->sortable(),
+
+            Column::make("Nombre", "medicineReserveFromUser.name")
+                ->sortable()
+                ->searchable(fn ($query, $searchTerm) => $query->orWhere('name', 'like', '%' . $searchTerm . '%')),
+
+            Column::make("SEDE", "user.userHeadquarter.name")
+                ->sortable(),
+            Column::make("Apellido Paterno", "userParticipantUser.apParental")
+                ->sortable(),
+
+            Column::make("Apellido Materno", "userParticipantUser.apMaternal")
+                ->sortable(),
+
+            Column::make("Tipo", "medicineReserveMedicine.medicineTypeExam.name")
+                ->sortable(),
+            Column::make("FECHA", "dateReserve")
+                ->sortable()
+                ->searchable(),
+
+            Column::make("HORA", "medicineSchedule.time_start")
+                ->sortable(),
+
+            Column::make("CURP", "userParticipantUser.curp")
+                ->sortable(),
+
+            Column::make("LLAVE DE PAGO", "medicineReserveMedicine.reference_number")
+                ->sortable(),
+            Column::make("Fecha de creación", "created_at")
+                ->sortable(),
+        ];
+    }
+    public function builder(): Builder
+    {
+        return MedicineReserve::query();
+    }
+
+    public function exportSelected()
+    {
+        if ($this->getSelected()) {
+            $query = MedicineReserve::with([
+                'medicineReserveMedicine', 'medicineReserveFromUser', 'user', 'userParticipantUser'
+            ])->whereIn('id', $this->getSelected());
+            $results = $query->get();
+            $job = new ExportSelectedJob($results);
+            dispatch($job); // Agregar el trabajo a la cola
+
+            
+        } else {
+            // Lógica para manejar el caso en el que no se hayan seleccionado registros
+        }
+    }
+}
--- app/Jobs/ExportSelectedJob.php
+++ app/Jobs/ExportSelectedJob.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
+use Jenssegers\Date\Date;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExportSelectedJob implements ShouldQueue
@@ -33,35 +34,12 @@ class ExportSelectedJob implements ShouldQueue
      */
     public function handle()
     {
-        // try {
-        //     $fileName = 'scheduled.xlsx';
-        //     // $path = storage_path('app/public/' . $fileName);
-        //     $filePath = 'uploads/citas-app/medicine/exports/scheduled.xlsx';
-
-        //     Excel::store(new ScheduledExport($this->results), $filePath,'do');
-
-        //     return $filePath;
-        // } catch (\Exception $e) {
-        //     dd($e->getMessage());
-        // }
-        // FUNCIONA PERO FALTA DESCARGAR
         try {
-            // $fileName = 'scheduled.xlsx';
-            $filePath = 'uploads/citas-app/medicine/exports/schedules.xlsx';
-
+            $date = Date::now();
+            $filePath = 'uploads/citas-app/medicine/exports/' . $date . '.xlsx';
             Excel::store(new ScheduledExport($this->results), $filePath, 'do');
-
-            // $url = Storage::disk('do')->url($filePath);
-            $fileContents = Storage::disk('do')->get($filePath);
-            // Storage::disk('do')->delete($filePath);
-            return response()->streamDownload(function () use ($fileContents) {
-                echo $fileContents;
-            }, 'schedules.xlsx');
         } catch (\Exception $e) {
-            dd($e->getMessage());
+            $e->getMessage();
         }
-
     }
-    }
-
-
+}
