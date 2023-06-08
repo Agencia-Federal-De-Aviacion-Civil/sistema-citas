52e9fa655549a71e05e76bc95f1f37016ca23769
jessicasoto01afac <jessica.soto928@gmail.com>
2023-06-08 13:36:06 -0600

    cambios

--- app/Notifications/MyVerifyEmail.php
+++ app/Notifications/MyVerifyEmail.php
@@ -4,7 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
-
+use Illuminate\Support\HtmlString;
 class MyVerifyEmail extends VerifyEmail
 {
     public function toMail($notifiable)
@@ -15,9 +15,13 @@ class MyVerifyEmail extends VerifyEmail
         if (static::$toMailCallback) {
             return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
         }
+
+        $someHtmlBody="<div align='center'><img src='https://citas.afac-avciv.com/images/logoafac.png'></div>";
+
         return (new MailMessage)
             ->subject('Verificación de Correo electronico')
-            ->greeting('Estimado(a)')
+            ->greeting(new HtmlString($someHtmlBody))
+            ->line('Estimado(a)')
             ->line('Porfavor dar clic en el botón para verificar tu correo electronico.')
             ->action('Verificar correo electronico', $verificationUrl)
             ->line('Si no creó una cuenta, no se requiere ninguna otra acción.')
