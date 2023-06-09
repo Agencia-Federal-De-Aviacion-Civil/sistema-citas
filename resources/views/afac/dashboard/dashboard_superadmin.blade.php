a3a5edfc195cccc965223d6d01b113387f0b3be4
jessicasoto01afac <jessica.soto928@gmail.com>
2023-06-09 10:19:53 -0600

    cambios

--- resources/views/afac/dashboard/dashboard_superadmin.blade.php
+++ resources/views/afac/dashboard/dashboard_superadmin.blade.php
@@ -5,11 +5,11 @@
     <div
         class="z-10 relative container px-6 mx-auto flex flex-col md:flex-row items-start md:items-center justify-between">
         <div>
-            <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">DASHBOARD</h4>
+            <h4 tabindex="0" class="focus:outline-none text-2xl font-bold leading-tight text-white">Dashboard</h4>
             <ul class="flex flex-col md:flex-row items-start md:items-center text-gray-300 text-sm mt-3">
                 <li class="flex items-center mt-4 md:mt-0">
                     <div class="mr-1">
-                        {{-- <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/background_with_sub_text-svg3.svg" alt="date"> --}}
+                        <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/background_with_sub_text-svg3.svg" alt="date">
                     </div>
                     {{-- <span tabindex="0" class="focus:outline-none">Started on 29 Jan 2020</span> --}}
                 </li>
