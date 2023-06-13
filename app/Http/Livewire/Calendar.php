0166a3b9036d31423b4e54a06471061f279b30ab
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-13 10:27:20 -0600

    se arregla el calendario

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Livewire;
+
 use Livewire\Component;
 use App\Models\Medicine\MedicineReserve;
 
@@ -8,19 +10,18 @@ class Calendar extends Component
     public $events = '';
 
     public function getevent()
-    {       
-        $events = MedicineReserve::select('id','medicine_id as title' ,'dateReserve as start')
-        ->where('status', 'in(0,1,4)')
-        ->get();
+    {
+        $events = MedicineReserve::select('id', 'medicine_id as title', 'dateReserve as start')
+            ->where('status', 'in(0,1,4)')
+            ->get();
         return  json_encode($events);
-        
     }
 
     /**
-    * Write code on Method
-    *
-    * @return response()
-    */ 
+     * Write code on Method
+     *
+     * @return response()
+     */
     public function addevent($event)
     {
         $input['medicine_id'] = $event['medicine_id'];
@@ -29,30 +30,43 @@ class Calendar extends Component
     }
 
     /**
-    * Write code on Method
-    *
-    * @return response()
-    */
+     * Write code on Method
+     *
+     * @return response()
+     */
     public function eventDrop($event, $oldEvent)
     {
-      $eventdata = MedicineReserve::find($event['id']);
-      $eventdata->start = $event['dateReserve'];
-      $eventdata->save();
+        $eventdata = MedicineReserve::find($event['id']);
+        $eventdata->start = $event['dateReserve'];
+        $eventdata->save();
     }
 
     /**
-    * Write code on Method
-    *
-    * @return response()
-    */
+     * Write code on Method
+     *
+     * @return response()
+     */
     public function render()
-    {       
-        $events = MedicineReserve::select('id','medicine_id as title' ,'dateReserve as start')
-        ->where('status', 'in(0,1,4)')
-        ->get();
-       // dd($events);
+    {
+
+        $queryEvents = MedicineReserve::with('user')
+            ->whereIn('status', [0, 1, 4])
+            ->get();
+
+        $groupedEvents = $queryEvents->groupBy(function ($event) {
+            return $event->user->name . '_' . $event->dateReserve;
+        });
+
+        $events = $groupedEvents->map(function ($events) {
+            $count = $events->count();
+
+            return [
+                'id' => $events->first()->id,
+                'title' => $events->first()->user->name . ' (' . $count . ')',
+                'start' => $events->first()->dateReserve,
+            ];
+        })->values();
         $this->events = json_encode($events);
         return view('livewire.calendar');
-
     }
 }
