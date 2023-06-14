cc3ffcd50c9a2b7771579af3843dd7b09b40557f
jessicasoto01afac <jessica.soto928@gmail.com>
2023-06-13 19:51:53 -0600

    estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/build.css') }}">
     <link rel="stylesheet" href="{{ asset('css/loader.css') }}">
     <link rel="stylesheet" href="{{ asset('css/upload.css') }}">
     @livewireStyles
