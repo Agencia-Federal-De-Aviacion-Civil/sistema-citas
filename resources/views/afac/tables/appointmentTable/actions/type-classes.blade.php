fc6ee69be1b808700c1694b855600aa687787af4
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:41:00 -0600

    s

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -3,9 +3,9 @@
         @if ($clas->medicineReserveMedicine->type_exam_id == 1)
             {{ $clas->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
         @elseif ($clas->medicineReserveMedicine->type_exam_id == 2)
-            {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
+            {{ $clas->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
         @elseif ($clas->medicineReserveMedicine->type_exam_id == 3)
-            {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
+            {{ $clas->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
         @endif
     @endforeach
 </div>
