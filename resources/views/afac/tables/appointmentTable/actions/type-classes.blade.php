493ede2d91cb07366b49f8a8d3f5cdf53923fcf1
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:43:27 -0600

    error

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -2,10 +2,13 @@
     @foreach ($class as $clas)
         @if ($clas->medicineReserveMedicine->type_exam_id == 1)
             {{ $clas->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
+            1
         @elseif ($clas->medicineReserveMedicine->type_exam_id == 2)
             {{ $clas->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
+            2
         @elseif ($clas->medicineReserveMedicine->type_exam_id == 3)
-            {{ $clas->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
+            {{-- {{ $clas->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }} --}}
+            3
         @endif
     @endforeach
 </div>
