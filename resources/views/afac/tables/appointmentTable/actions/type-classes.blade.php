7f4c594c6d360ba2e88ce14c99276af3f847111b
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:16:20 -0600

    se arregla la variable

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -1,12 +1,12 @@
 <div>
-    @foreach ($classes as $class)
-        @if ($class->medicineReserveMedicine->medicineTypeExam->id == 1)
+    @foreach ($class as $clas)
+        @if ($clas->medicineReserveMedicine->medicineTypeExam->id == 1)
             ok
             {{-- {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }} --}}
-        @elseif ($class->medicineReserveMedicine->medicineTypeExam->id == 2)
+        @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 2)
             {{-- {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }} --}}
             ok
-        @elseif ($class->medicineReserveMedicine->medicineTypeExam->id == 3)
+        @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 3)
             {{-- {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }} --}}
             ok
         @endif
