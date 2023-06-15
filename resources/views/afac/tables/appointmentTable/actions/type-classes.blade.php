c27afd954a1babad71e27c5e5bcad41793df7dc5
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:39:34 -0600

    se manda el id

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -2,11 +2,10 @@
     @foreach ($class as $clas)
         @if ($clas->medicineReserveMedicine->type_exam_id == 1)
             {{ $clas->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
-        @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 2)
-            {{-- {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }} --}}
-        @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 3)
-            {{-- {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
-            3 --}}
+        @elseif ($clas->medicineReserveMedicine->type_exam_id == 2)
+            {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
+        @elseif ($clas->medicineReserveMedicine->type_exam_id == 3)
+            {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
         @endif
     @endforeach
 </div>
