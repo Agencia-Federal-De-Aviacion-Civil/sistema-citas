97c069dd6ce511ebf98eabd95211447416553794
jessicasoto01afac <jessica.soto928@gmail.com>
2023-06-15 13:32:38 -0600

    diseño de la alerta de banner de descraga

--- resources/views/livewire/medicine/appointment-medicine.blade.php
+++ resources/views/livewire/medicine/appointment-medicine.blade.php
@@ -42,8 +42,28 @@
                             </div>
                         @endif
                         @if ($exportFinished)
-                            Listo. Descarga el documento <button class="hover:underline"
-                                wire:click="downloadExport">AQUI.</button>
+                                <div x-data="{ open: false }"  x-init="() => setTimeout(() => open = true, 500)">
+                                    <div  x-show="open"     
+                                          x-transition:enter-start="opacity-0 scale-90" 
+                                          x-transition:enter="transition duration-200 transform ease"
+                                          x-transition:leave="transition duration-200 transform ease"
+                                          x-transition:leave-end="opacity-0 scale-90"
+                                           class="max-w-screen-lg mx-auto fixed bg-sky-900 inset-x-5 p-5 bottom-40 rounded-lg drop-shadow-2xl flex gap-4 flex-wrap md:flex-nowrap text-center md:text-left items-center justify-center md:justify-between">
+                                      <div class="w-full text-white flex">
+                                        <div class="flex-shrink-0">
+                                            <svg class="h-5 w-5 text-teal-400 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                                            </svg>
+                                          </div>
+                                          <div class="ml-3">
+                                        <span class=""> Listo!</span> El documento ya se encuentra disponible para su descarga.
+                                        <a href="#" class="text-indigo-600 whitespace-nowrap  hover:underline"></a></div></div>
+                                      <div class="flex gap-4 items-center flex-shrink-0">
+                                        <button @click="open = false" class="text-indigo-300 focus:outline-none hover:underline">Cerrar</button>
+                                        <button wire:click="downloadExport" @click="open = false" class="bg-indigo-500 px-5 py-2 text-white rounded-md hover:bg-indigo-700 focus:outline-none">Descargar</button>
+                                      </div>
+                                    </div>
+                                  </div>
                         @endif
                     </div>
                     @livewire('medicine.tables.appointment-table')
