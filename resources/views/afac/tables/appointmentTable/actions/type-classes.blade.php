08e0517366ef1752d3b46415ffae77eee817123f
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:22:22 -0600

    se manda posible error

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -3,10 +3,10 @@
         @if ($clas->medicineReserveMedicine->medicineTypeExam->id == 1)
             1{{-- {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }} --}}
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 2)
-            {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
+            {{-- {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }} --}}
             
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 3)
-            {{-- {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }} --}}
+            {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
             3
         @endif
     @endforeach
