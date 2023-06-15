6a3fcc61300cf0f59c21956a48a490e39ff70f7a
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:19:58 -0600

    se comenta bien

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -1,13 +1,13 @@
 <div>
     @foreach ($class as $clas)
         @if ($clas->medicineReserveMedicine->medicineTypeExam->id == 1)
-            {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
+            1{{-- {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }} --}}
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 2)
-            {{-- {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }} --}}
-            ok
+            {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
+            
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 3)
             {{-- {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }} --}}
-            ok
+            3
         @endif
     @endforeach
 </div>
