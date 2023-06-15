a88da7daacd6777ae8c86a59b7ac0b05bdd35984
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:36:27 -0600

    me sobro una s

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -1,7 +1,7 @@
 <div>
     @foreach ($class as $clas)
         @if ($clas->medicineReserveMedicine->type_exam_id == 1)
-            {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
+            {{ $clas->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 2)
             {{-- {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }} --}}
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 3)
