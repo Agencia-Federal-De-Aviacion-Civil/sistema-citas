7ecec9d89c7735201b890f913ccfcf68bc67980d
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:54:21 -0600

    tipo de clases

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -2,13 +2,14 @@
     @foreach ($class as $clas)
         @if ($clas->medicineReserveMedicine->type_exam_id == 1)
             {{ $clas->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }}
-            1
         @elseif ($clas->medicineReserveMedicine->type_exam_id == 2)
             {{ $clas->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }}
-            2
-        @elseif ($clas->medicineReserveMedicine->type_exam_id == 3)
-            {{-- {{ $clas->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }} --}}
-            3
+        @else
+            @if ($clas->medicineReserveMedicine->medicineRevaluation[0]->type_exam_id == 1)
+                {{ $clas->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
+            @else
+                {{ $clas->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationTypeClass->name }}
+            @endif
         @endif
     @endforeach
 </div>
