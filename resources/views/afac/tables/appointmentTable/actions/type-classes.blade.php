fc0e1c49b6c5f810db101e2730d0c9fa56b5c00e
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-06-15 11:28:55 -0600

    se modifica la consulta

--- resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/type-classes.blade.php
@@ -1,13 +1,14 @@
 <div>
     @foreach ($class as $clas)
         @if ($clas->medicineReserveMedicine->medicineTypeExam->id == 1)
-            1{{-- {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }} --}}
+            {{-- {{ $class->medicineReserveMedicine->medicineInitial[0]->medicineInitialTypeClass->name }} --}}
+            {{$clas->medicineReserveMedicine->medicineInitial[0]->medicineInitialClasificationClass->name}}
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 2)
             {{-- {{ $class->medicineReserveMedicine->medicineRenovation[0]->renovationTypeClass->name }} --}}
             
         @elseif ($clas->medicineReserveMedicine->medicineTypeExam->id == 3)
-            {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
-            3
+            {{-- {{ $class->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineInitial[0]->revaluationInitialTypeClass->name }}
+            3 --}}
         @endif
     @endforeach
 </div>
