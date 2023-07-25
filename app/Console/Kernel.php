c1b3bc1f4ab5666beac7bbf0d3ce68973d5e74f3
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-07-25 17:46:26 -0600

    se actuliza el horario de backups

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,9 +16,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('backup:clean')->daily()->at('08:00');
-        $schedule->command('backup:run')->daily()->at('09:00');
-        $schedule->command('backup:monitor')->daily()->at('11:00');
+        $schedule->command('backup:clean')->daily()->at('13:00');
+        $schedule->command('backup:run')->daily()->at('15:00');
+        $schedule->command('backup:monitor')->daily()->at('17:00');
     }
 
     /**
