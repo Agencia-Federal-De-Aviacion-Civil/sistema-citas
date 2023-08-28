fdbd66b0d16e993d77b711713c4eaceac99f8619
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-08-28 15:02:06 -0600

    se agrega funcionalidad para medicos terceros

--- app/Http/Controllers/AppointmentMedicineController.php
+++ app/Http/Controllers/AppointmentMedicineController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Medicine\MedicineReserve;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Jenssegers\Date\Date;
 use PDF;
@@ -27,18 +26,28 @@ class AppointmentMedicineController extends Controller
         $dateAppointment = $medicineReserves[0]->dateReserve;
         $dateConvertedFormatted = Date::parse($dateAppointment)->format('l j F Y');
         $curp = $medicineReserves[0]->medicineReserveMedicine->medicineUser->userParticipant->pluck('curp')->first();
-        $keyEncrypt =  Crypt::encryptString($medicineId . '*' . $dateAppointment . '*' . $curp);
+        $keyEncrypt = Crypt::encryptString($medicineId . '*' . $dateAppointment . '*' . $curp);
         $idExternalInternal = boolval($medicineReserves[0]->is_external);
         if (!$idExternalInternal) {
             $fileName = $medicineReserves[0]->dateReserve . '-' . $curp . '-' . 'MED-' . $medicineId . '.pdf';
         } else {
             $fileName = $medicineReserves[0]->dateReserve . '-' . $curp . '-' . 'MED-EXT-' . $medicineId . '.pdf';
         }
+        $thirdAppointment = MedicineReserve::where('headquarter_id', $medicineReserves[0]->headquarter_id)
+            ->where('dateReserve', $medicineReserves[0]->dateReserve)
+            ->where(function ($query) {
+                $query->where('status', 0)
+                    ->Orwhere('status', 1);
+            })
+            ->where(function ($query2) {
+                $query2->where('is_external', true);
+            })
+            ->count();
         if ($medicineReserves[0]->medicineReserveMedicine->type_exam_id == 1) {
-            $pdf = PDF::loadView('livewire.medicine.documents.medicine-initial', compact('medicineReserves', 'keyEncrypt', 'dateConvertedFormatted', 'idExternalInternal'));
+            $pdf = PDF::loadView('livewire.medicine.documents.medicine-initial', compact('medicineReserves', 'keyEncrypt', 'dateConvertedFormatted', 'idExternalInternal','thirdAppointment'));
             return $pdf->download($fileName);
         } else if ($medicineReserves[0]->medicineReserveMedicine->type_exam_id == 2) {
-            $pdf = PDF::loadView('livewire.medicine.documents.medicine-renovation', compact('medicineReserves', 'keyEncrypt', 'dateConvertedFormatted', 'idExternalInternal'));
+            $pdf = PDF::loadView('livewire.medicine.documents.medicine-renovation', compact('medicineReserves', 'keyEncrypt', 'dateConvertedFormatted', 'idExternalInternal','thirdAppointment'));
             return $pdf->download($fileName);
         } else if ($medicineReserves[0]->medicineReserveMedicine->type_exam_id == 3) {
             $pdf = PDF::loadView('livewire.medicine.documents.medicine-revaluation', compact('medicineReserves', 'keyEncrypt', 'dateConvertedFormatted'));
