079d0bdbc84fe1eda5440750c8a151a61fdacf77
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-09-01 16:11:09 -0600

    SE MANDA CONTROLADOR

--- app/Http/Controllers/Api/Medicine/UsersController.php
+++ app/Http/Controllers/Api/Medicine/UsersController.php
@@ -8,9 +8,48 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function users()
+    public function create(Request $request)
     {
-        $usersAfac = User::all();
-        return response()->json($usersAfac);
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        $users = new User();
+        $users->name = $request->name;
+        $users->email = $request->email;
+        $users->password = $request->password;
+
+        $users->save();
+
+        return response([
+            "status" => 1,
+            "message" => "éxitoso"
+        ]);
+    }
+    public function list(Request $request)
+    {
+        if ($request->header('Authorization') === 'Bearer 2|4ZWgUwhajRaJQyQv40tBvMVHzh1YU1EWN9GMTtGZ') {
+            $userList = User::all();
+            return response([
+                "status" => 1,
+                "message" => "Lista de usuarios",
+                "data" => $userList
+            ]);
+        } else {
+            return response([
+                "status" => 0,
+                "message" => "No autorizado",
+            ], 401);
+        }
+    }
+    public function show()
+    {
+    }
+    public function update()
+    {
+    }
+    public function delete()
+    {
     }
 }
