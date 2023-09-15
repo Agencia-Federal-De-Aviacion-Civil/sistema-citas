feb918210e542ed3764bce2943c920759fbf4e9a
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-09-15 15:24:51 -0600

    se manda moficiación en la migracion

--- database/migrations/2023_09_14_114518_add_state_priece_to_headquaters.php
+++ database/migrations/2023_09_14_114518_add_state_priece_to_headquaters.php
@@ -27,7 +27,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('headquaters', function (Blueprint $table) {
-            //
+            $table->dropColumn('state');
+            $table->dropColumn('price');
         });
     }
 };
