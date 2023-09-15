210135f17464778dcb9bc454db1d996be0111eb4
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-09-15 15:33:47 -0600

    se comenta la tabla

--- database/migrations/2023_09_14_114518_add_state_priece_to_headquaters.php
+++ database/migrations/2023_09_14_114518_add_state_priece_to_headquaters.php
@@ -29,6 +29,7 @@ return new class extends Migration
         Schema::table('headquaters', function (Blueprint $table) {
             $table->dropColumn('state');
             $table->dropColumn('price');
+            // AQUI
         });
     }
 };
