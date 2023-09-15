a8d0fd4f829f2af0eef1f38b93be51ffd7412dc2
jessicasoto01afac <jessica.soto928@gmail.com>
2023-09-15 16:29:01 -0600

    dasboard

--- app/Http/Livewire/Medicine/AuthorizedThird/HomeMedicineAuthorizedThird.php
+++ app/Http/Livewire/Medicine/AuthorizedThird/HomeMedicineAuthorizedThird.php
@@ -14,11 +14,11 @@ class HomeMedicineAuthorizedThird extends Component
 
     public function render()
     {
-        $date = Date::now()->format('l j F Y');
-        $date1 = Date::now()->format('Y-m-d');
-        $date2 = Date::now()->format('d-m-Y');
-        $tomorrow = Date::tomorrow()->format('Y-m-d');
-        $nameHeadquarter = '';
+        $date1_third = Date::now()->format('l j F Y');
+        $date1_third = Date::now()->format('Y-m-d');
+        $date2_third = Date::now()->format('d-m-Y');
+        $tomorrow_third = Date::tomorrow()->format('Y-m-d');
+        $nameHeadquarter_third = '';
         if (Auth::user()->can('headquarters.see.dashboard')) {
             $appointment = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
                 ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q1) {
@@ -27,14 +27,14 @@ class HomeMedicineAuthorizedThird extends Component
                 ->select('status', DB::raw('count(*) as count'), 'dateReserve')
                 ->groupBy('status', 'dateReserve')
                 ->get();
-            $headquarters = Headquarter::with([
+            $headquarters_third = Headquarter::with([
                 'HeadquarterUserHeadquarter.userHeadquarterUserParticipant'
             ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
                 $q2->where('user_id', Auth::user()->id);
             })->get();
-            $nameHeadquarter = $headquarters->pluck('name_headquarter')->first();
+            $nameHeadquarter_third = $headquarters_third->pluck('name_headquarter')->first();
         } else if (Auth::user()->can('sub_headquarters.see.dashboard')) {
-            $appointment = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
+            $appointment_third = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
                 ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q3) {
                     $q3->where('user_id', Auth::user()->id);
                 })
@@ -43,51 +43,51 @@ class HomeMedicineAuthorizedThird extends Component
                 ->where('headquarter_id', 6)
                 ->where('dateReserve', $date1)
                 ->get();
-            $headquarters = Headquarter::with([
+            $headquarters_third = Headquarter::with([
                 'HeadquarterUserHeadquarter.userHeadquarterUserParticipant'
             ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
                 $q2->where('user_id', Auth::user()->id);
             })->get();
         } else if (Auth::user()->can('headquarters_authorized.see.dashboard')) {
-            $appointment = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
+            $appointment_third = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
                 ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q1) {
                     $q1->where('user_id', Auth::user()->id);
                 })
                 ->select('status', DB::raw('count(*) as count'), 'dateReserve')
                 ->groupBy('status', 'dateReserve')
                 ->get();
-            $headquarters = Headquarter::with([
+            $headquarters_third = Headquarter::with([
                 'HeadquarterUserHeadquarter.userHeadquarterUserParticipant','headquarterMedicineReserve'
             ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
                 $q2->where('user_id', Auth::user()->id);
             })->where('is_external', true)->get();
-            $nameHeadquarter = '"'. $headquarters->pluck('name_headquarter')->first().'"';
+            $nameHeadquarter_third = '"'. $headquarters->pluck('name_headquarter')->first().'"';
         } else {
-            $appointment = MedicineReserve::query()
+            $appointment_third = MedicineReserve::query()
                 ->select('status', DB::raw('count(*) as count'), 'dateReserve')
                 ->groupBy('status', 'dateReserve')
                 ->where('is_external', true)
                 ->get();
-            $headquarters = Headquarter::with([
+            $headquarters_third = Headquarter::with([
                 'headquarterMedicineReserve'
             ])
                 ->where('is_external', true)
                 ->get();
-            $nameHeadquarter = 'TERCEROS';
+            $nameHeadquarter_third = 'TERCEROS';
         }
 
-        $appointmentNow = $appointment->where('dateReserve', $date1);
-        $now = $appointmentNow->whereIn('status', ['0', '1', '4'])->sum('count');
-        $registradas = $appointment->sum('count');
-        $porconfir = $registradas != 0 ? round($appointment->where('status', '1')->sum('count') * 100 / $registradas, 0) : 0;
-        $validado = $appointment->where('status', '1')->sum('count');
-        $pendientes = $appointment->where('status', '0')->sum('count');
-        $porpendientes = $registradas != 0 ? round($appointment->where('status', '0')->sum('count') * 100 / $registradas, 0) : 0;
-        $canceladas = $appointment->whereIn('status', ['2', '3', '5'])->sum('count');
-        $reagendado = round($appointment->where('status', '4')->sum('count'));
-        $porreagendado = $registradas != 0 ? round($appointment->where('status', '4')->sum('count') * 100 / $registradas) : 0;
-        $porcanceladas = $registradas != 0 ? round($appointment->whereIn('status', ['2', '3', '5'])->sum('count') * 100 / $registradas, 0) : 0;
-        $medicine =  round($registradas ? $registradas * 100 / $registradas : '0');
+        $appointmentNow_third = $appointment_third->where('dateReserve', $date1_third);
+        $now_third = $appointmentNow_third->whereIn('status', ['0', '1', '4'])->sum('count');
+        $registradas_third = $appointment_third->sum('count');
+        $porconfir_third = $registradas_third != 0 ? round($appointment_third->where('status', '1')->sum('count') * 100 / $registradas_third, 0) : 0;
+        $validado_third = $appointment_third->where('status', '1')->sum('count');
+        $pendientes_third = $appointment_third->where('status', '0')->sum('count');
+        $porpendientes_third = $registradas_third != 0 ? round($appointment_third->where('status', '0')->sum('count') * 100 / $registradas_third, 0) : 0;
+        $canceladas_third = $appointment_third->whereIn('status', ['2', '3', '5'])->sum('count');
+        $reagendado_third = round($appointment->where('status', '4')->sum('count'));
+        $porreagendado_third = $registradas_third != 0 ? round($appointment_third->where('status', '4')->sum('count') * 100 / $registradas_third) : 0;
+        $porcanceladas_third = $registradas_third != 0 ? round($appointment_third->whereIn('status', ['2', '3', '5'])->sum('count') * 100 / $registradas_third, 0) : 0;
+        $medicine_third =  round($registradas_third ? $registradas_third * 100 / $registradas_third : '0');
         return view('livewire.medicine.authorized-third.home-medicine-authorized-third', compact('headquarters', 'nameHeadquarter', 'registradas', 'pendientes', 'validado', 'canceladas', 'reagendado', 'porconfir', 'porpendientes', 'porreagendado', 'porcanceladas', 'now', 'date', 'date2', 'medicine', 'date1', 'tomorrow'));
     }
 }
