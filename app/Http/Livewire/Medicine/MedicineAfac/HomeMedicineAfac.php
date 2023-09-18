e58c3ab460b61be8bab6793b4d715e1fdb4832ed
jessicasoto01afac <jessica.soto928@gmail.com>
2023-09-18 14:14:33 -0600

    prueba dashboard

--- app/Http/Livewire/Medicine/MedicineAfac/HomeMedicineAfac.php
+++ app/Http/Livewire/Medicine/MedicineAfac/HomeMedicineAfac.php
@@ -21,36 +21,36 @@ class HomeMedicineAfac extends Component
         $date2_afac = Date::now()->format('d-m-Y');
         $tomorrow_afac = Date::tomorrow()->format('Y-m-d');
         $nameHeadquarter_afac = '';
-        if (Auth::user()->can('headquarters.see.dashboard')) {
-            $appointment_afac = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
-                ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q1) {
-                    $q1->where('user_id', Auth::user()->id);
-                })
-                ->select('status', DB::raw('count(*) as count'), 'dateReserve')
-                ->groupBy('status', 'dateReserve')
-                ->get();
-            $headquarters_afac = Headquarter::with([
-                'HeadquarterUserHeadquarter.userHeadquarterUserParticipant'
-            ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
-                $q2->where('user_id', Auth::user()->id);
-            })->get();
-            $nameHeadquarter_afac = $headquarters_afac->pluck('name_headquarter')->first();
-        } else if (Auth::user()->can('sub_headquarters.see.dashboard')) {
-            $appointment_afac = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
-                ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q3) {
-                    $q3->where('user_id', Auth::user()->id);
-                })
-                ->select('status', DB::raw('count(*) as count'), 'dateReserve')
-                ->groupBy('status', 'dateReserve')
-                ->where('headquarter_id', 6)
-                ->where('dateReserve', $date1_afac)
-                ->get();
-            $headquarters_afac = Headquarter::with([
-                'HeadquarterUserHeadquarter.userHeadquarterUserParticipant'
-            ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
-                $q2->where('user_id', Auth::user()->id);
-            })->get();
-        } else {
+        // if (Auth::user()->can('headquarters.see.dashboard')) {
+        //     $appointment_afac = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
+        //         ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q1) {
+        //             $q1->where('user_id', Auth::user()->id);
+        //         })
+        //         ->select('status', DB::raw('count(*) as count'), 'dateReserve')
+        //         ->groupBy('status', 'dateReserve')
+        //         ->get();
+        //     $headquarters_afac = Headquarter::with([
+        //         'HeadquarterUserHeadquarter.userHeadquarterUserParticipant'
+        //     ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
+        //         $q2->where('user_id', Auth::user()->id);
+        //     })->get();
+        //     $nameHeadquarter_afac = $headquarters_afac->pluck('name_headquarter')->first();
+        // } else if (Auth::user()->can('sub_headquarters.see.dashboard')) {
+        //     $appointment_afac = MedicineReserve::with('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant')
+        //         ->whereHas('medicineReserveHeadquarter.HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q3) {
+        //             $q3->where('user_id', Auth::user()->id);
+        //         })
+        //         ->select('status', DB::raw('count(*) as count'), 'dateReserve')
+        //         ->groupBy('status', 'dateReserve')
+        //         ->where('headquarter_id', 6)
+        //         ->where('dateReserve', $date1_afac)
+        //         ->get();
+        //     $headquarters_afac = Headquarter::with([
+        //         'HeadquarterUserHeadquarter.userHeadquarterUserParticipant'
+        //     ])->whereHas('HeadquarterUserHeadquarter.userHeadquarterUserParticipant', function ($q2) {
+        //         $q2->where('user_id', Auth::user()->id);
+        //     })->get();
+        // } else {
             $appointment_afac = MedicineReserve::query()
                 ->select('status', DB::raw('count(*) as count'), 'dateReserve')
                 ->groupBy('status', 'dateReserve')
@@ -61,7 +61,7 @@ class HomeMedicineAfac extends Component
             ])
                 ->where('is_external', false)
                 ->get();
-        }
+        // }
         $appointmentNow_afac = $appointment_afac->where('dateReserve', $date1_afac);
         $now_afac = $appointmentNow_afac->whereIn('status', ['0', '1', '4'])->sum('count');
         $registradas_afac = $appointment_afac->sum('count');
@@ -75,6 +75,7 @@ class HomeMedicineAfac extends Component
         $porcanceladas_afac = $registradas_afac != 0 ? round($appointment_afac->whereIn('status', ['2', '3', '5'])->sum('count') * 100 / $registradas_afac, 0) : 0;
         // $medicine_afac =  round($registradas_afac ? $registradas_afac * 100 / $registradas_afac : '0');
         // return view('livewire.medicine.medicine-afac.home-medicine-afac', compact('headquarters_afac', 'nameHeadquarter_afac', 'registradas_afac', 'pendientes_afac', 'validado_afac', 'canceladas_afac', 'reagendado_afac', 'porconfir_afac', 'porpendientes_afac', 'porreagendado_afac', 'porcanceladas_afac', 'now_afac', 'date_afac', 'date2_afac', 'medicine_afac', 'date1_afac', 'tomorrow_afac', 'dateNow_afac'));
-        return view('livewire.medicine.medicine-afac.home-medicine-afac', compact('now_afac', 'registradas_afac', 'porconfir_afac', 'validado_afac', 'pendientes_afac', 'porpendientes_afac', 'canceladas_afac', 'reagendado_afac', 'porreagendado_afac', 'porcanceladas_afac'));
+        //return view('livewire.medicine.medicine-afac.home-medicine-afac', compact('now_afac', 'registradas_afac', 'porconfir_afac', 'validado_afac', 'pendientes_afac', 'porpendientes_afac', 'canceladas_afac', 'reagendado_afac', 'porreagendado_afac', 'porcanceladas_afac'));
+        return view('livewire.medicine.medicine-afac.home-medicine-afac', compact('now_afac', 'registradas_afac', 'porconfir_afac', 'validado_afac', 'pendientes_afac', 'porpendientes_afac', 'canceladas_afac', 'reagendado_afac', 'porreagendado_afac', 'porcanceladas_afac','date2_afac','headquarters_afac','date1_afac','tomorrow_afac'));
     }
 }
--- resources/views/livewire/medicine/medicine-afac/home-medicine-afac.blade.php
+++ resources/views/livewire/medicine/medicine-afac/home-medicine-afac.blade.php
@@ -74,7 +74,7 @@
                 </div>
             </div>
         </div>
-        {{-- <div class="grid grid-cols-1 lg:grid-cols-1 p-2 gap-4">
+        <div class="grid grid-cols-1 lg:grid-cols-1 p-2 gap-4">
             <div
                 class="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
                 <div class="rounded-t mb-0 px-0 border-0">
@@ -165,7 +165,7 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
         <div class="py-12">
             <div class="container mx-auto px-4 py-4 bg-white sm:rounded-lg">
                 <div class="mt-12 md:max-w-8xl  xs:max-w-4xl  mx-auto sm:px-6 lg:px-8">
