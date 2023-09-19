3c12774c4841e798e7a69a97da9b497be71b1899
jessicasoto01afac <jessica.soto928@gmail.com>
2023-09-19 13:07:28 -0600

    arreglo de bug al cambiar de clase / generar citas

--- resources/views/livewire/medicine/home-medicine.blade.php
+++ resources/views/livewire/medicine/home-medicine.blade.php
@@ -365,7 +365,7 @@
                                                                 DE CLASE</label>
                                                             <select id="small" x-ref="clasification"
                                                                 placeholder="seleccione..."
-                                                                wire:model.lazy="type_class_id"
+                                                                wire:model.lazy="type_class_id" wire:change.prevent="cleanclass"
                                                                 class="block w-full p-2 mb-2 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                                                 <option value="">Seleccione...</option>
                                                                 @foreach ($typeRenovationExams as $typeRenovationExam)
