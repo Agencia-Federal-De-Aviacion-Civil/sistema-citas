6b0851d12f3b06fb0b5e70a1baf90cf04d4010a7
jessicasoto01afac <jessica.soto928@gmail.com>
2023-09-25 16:25:33 -0600

    Bug_terceros_autorizados_citas_multiselect

--- resources/views/livewire/medicine/home-medicine.blade.php
+++ resources/views/livewire/medicine/home-medicine.blade.php
@@ -10,11 +10,11 @@
     @if ($idTypeAppointment === false)
         @livewire('medicine.modals.modal-index')
     @endif
-    @unless ($showBannerBoolean)
+    {{-- @unless ($showBannerBoolean) --}}
         <x-banner-component :title="$idTypeAppointment === false
             ? 'Generación de citas medicina de Aviación AFAC'
             : 'Generación de citas medicina de Aviación Terceros'" />
-    @endunless
+    {{-- @endunless --}}
     <div class="py-12">
         <div class="container mx-auto px-4 py-4 bg-white shadow-xl sm:rounded-lg">
             <div class="mt-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
