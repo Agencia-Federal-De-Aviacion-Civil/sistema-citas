536a6a9e28017a46fa06c2129c3d3b75e467c623
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-09-27 16:41:12 -0600

    se oculta el botón de guardar

--- resources/views/livewire/medicine/modals/medicine-extension-modal.blade.php
+++ resources/views/livewire/medicine/modals/medicine-extension-modal.blade.php
@@ -5,7 +5,6 @@
                 <h3 class="text-xl font-semibold leading-6 text-gray-800 capitalize dark:text-white" id="modal-title">
                     {{-- {{ $reference_number_ext ? 'EXTENSIÓN DE CITA PARA EL USUARIO' . '' . $extensionCurp : 'AÑADIR EXTENSIÓN' }} --}}
                 </h3>
-                <x-errors></x-errors>
                 @if (count($medicineReservesExtension[0]->medicineReserveMedicineExtension) > 0)
                     @if ($medicineReservesExtension[0]->medicineReserveMedicineExtension[0]->reference_number_ext)
                         <div class="mt-6 grid xl:grid-cols-2 xl:gap-6">
@@ -217,10 +216,12 @@
                     </div>
                 @endif
                 <div class="flex items-center justify-between w-full gap-4 mt-6">
-                    <button wire:click.prevent="saveExtension()"
-                        class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                        ACEPTAR
-                    </button>
+                    @unless ($medicineReservesExtension[0]->medicineReserveMedicineExtension[0]->reference_number_ext)
+                        <button wire:click.prevent="saveExtension()"
+                            class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                            ACEPTAR
+                        </button>
+                    @endunless
                     <button wire:click="$emit('closeModal')"
                         class="py-2 px-4  bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-indigo-500 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                         CERRAR
