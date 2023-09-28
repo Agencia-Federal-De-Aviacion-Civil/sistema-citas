072ec70bb14c17462fed14ba0194e3bfcbc1b426
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-09-28 10:04:28 -0600

    se manda el bloquedo del boton

--- resources/views/components/medicine/appointment-actions-component.blade.php
+++ resources/views/components/medicine/appointment-actions-component.blade.php
@@ -1,20 +1,22 @@
 <div>
     @if ($status == 0)
         @hasrole('sub_headquarters|super_admin|medicine_admin|super_admin_medicine|headquarters|admin_medicine_v2|headquarters_authorized')
-            @if (count($medicineExtensionNothing) > 0)
-                @if (is_null($medicineExtensionExist))
-                    <x-button
-                        wire:click="$emit('openModal', 'medicine.modals.medicine-extension-modal',{{ json_encode(['scheduleId' => $scheduleId]) }})"
-                        label="COMPLETAR EXT." xs blue right-icon="cash" />
+            @if ($showExpireButton)
+                @if (count($medicineExtensionNothing) > 0)
+                    @if (is_null($medicineExtensionExist))
+                        <x-button
+                            wire:click="$emit('openModal', 'medicine.modals.medicine-extension-modal',{{ json_encode(['scheduleId' => $scheduleId]) }})"
+                            label="COMPLETAR EXT." xs blue right-icon="cash" />
+                    @else
+                        <x-button
+                            wire:click="$emit('openModal', 'medicine.modals.medicine-extension-modal',{{ json_encode(['scheduleId' => $scheduleId]) }})"
+                            label="EXT. CONCLUÍDA" xs lime right-icon="cash" />
+                    @endif
                 @else
                     <x-button
                         wire:click="$emit('openModal', 'medicine.modals.medicine-extension-modal',{{ json_encode(['scheduleId' => $scheduleId]) }})"
-                        label="EXT. CONCLUÍDA" xs lime right-icon="cash" />
+                        label="AÑADIR EXT." xs blue right-icon="cash" />
                 @endif
-            @else
-                <x-button
-                    wire:click="$emit('openModal', 'medicine.modals.medicine-extension-modal',{{ json_encode(['scheduleId' => $scheduleId]) }})"
-                    label="AÑADIR EXT." xs blue right-icon="cash" />
             @endif
             <x-button
                 wire:click="$emit('openModal', 'medicine.modals.schedule', {{ json_encode(['scheduleId' => $scheduleId, 'medicineId' => $medicineId]) }})"
--- resources/views/livewire/medicine/modals/medicine-extension-modal.blade.php
+++ resources/views/livewire/medicine/modals/medicine-extension-modal.blade.php
@@ -216,12 +216,10 @@
                     </div>
                 @endif
                 <div class="flex items-center justify-between w-full gap-4 mt-6">
-                    @unless ($medicineReservesExtension[0]->medicineReserveMedicineExtension[0]->reference_number_ext)
-                        <button wire:click.prevent="saveExtension()"
-                            class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                            ACEPTAR
-                        </button>
-                    @endunless
+                    <button wire:click.prevent="saveExtension()"
+                        class="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                        ACEPTAR
+                    </button>
                     <button wire:click="$emit('closeModal')"
                         class="py-2 px-4  bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-indigo-500 w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                         CERRAR
