d40daff4707d15e59eeaa854e030ea7953da2778
angelcansecoc <angelcanseco.c@gmail.com>
2023-10-02 01:26:35 -0600

    Se codifica Contorlador Home Medicne 9

--- app/Http/Livewire/Medicine/MedicineAfac/HomeMedicineAfac.php
+++ app/Http/Livewire/Medicine/MedicineAfac/HomeMedicineAfac.php
@@ -16,11 +16,11 @@ class HomeMedicineAfac extends Component
     public function render()
     {
         Date::setLocale('es');
-        $dateNow_afac1 = Date::now()->format('l j F Y');
-        $date_afac1 = Date::now()->format('l j F Y');
+        // $dateNow_afac1 = Date::now()->format('l j F Y');
+        // $date_afac1 = Date::now()->format('l j F Y');
         $date1_afac1 = Date::now()->format('Y-m-d');
         $date2_afac1 = Date::now()->format('d-m-Y');
-        $tomorrow_afac1 = Date::tomorrow()->format('Y-m-d');
+        // $tomorrow_afac1 = Date::tomorrow()->format('Y-m-d');
 
         // $nameHeadquarter_afac1 = '';
         // if (Auth::user()->can('headquarters.see.dashboard')) {
@@ -54,22 +54,22 @@ class HomeMedicineAfac extends Component
         //     })->get();
         // } else {
 
-            $appointment_afac1 = MedicineReserve::query()
-                ->select('status', DB::raw('count(*) as count'), 'dateReserve')
-                ->groupBy('status', 'dateReserve')
-                ->where('is_external', false)
-                ->get();
-            $this->headquarters_afac1 = Headquarter::with([
-                'headquarterMedicineReserve'
-            ])
-                ->where('is_external', false)
-                ->get();
+            // $appointment_afac1 = MedicineReserve::query()
+            //     ->select('status', DB::raw('count(*) as count'), 'dateReserve')
+            //     ->groupBy('status', 'dateReserve')
+            //     ->where('is_external', false)
+            //     ->get();
+            // $this->headquarters_afac1 = Headquarter::with([
+            //     'headquarterMedicineReserve'
+            // ])
+            //     ->where('is_external', false)
+            //     ->get();
 
 
         // }
 
 
-        $appointmentNow_afac = $appointment_afac1->where('dateReserve', $date1_afac1);
+        // $appointmentNow_afac = $appointment_afac1->where('dateReserve', $date1_afac1);
         // $now_afac = $appointmentNow_afac->whereIn('status', ['0', '1', '4'])->sum('count');
         // $registradas_afac = $appointment_afac1->sum('count');
         // $porconfir_afac = $registradas_afac != 0 ? round($appointment_afac1->where('status', '1')->sum('count') * 100 / $registradas_afac, 0) : 0;
@@ -82,6 +82,6 @@ class HomeMedicineAfac extends Component
         // $porcanceladas_afac = $registradas_afac != 0 ? round($appointment_afac1->whereIn('status', ['2', '3', '5'])->sum('count') * 100 / $registradas_afac, 0) : 0;
         // $medicine_afac =  round($registradas_afac ? $registradas_afac * 100 / $registradas_afac : '0');
         // return view('livewire.medicine.medicine-afac.home-medicine-afac', compact('headquarters_afac', 'nameHeadquarter_afac', 'registradas_afac', 'pendientes_afac', 'validado_afac', 'canceladas_afac', 'reagendado_afac', 'porconfir_afac', 'porpendientes_afac', 'porreagendado_afac', 'porcanceladas_afac', 'now_afac', 'date_afac', 'date2_afac', 'medicine_afac', 'date1_afac1', 'tomorrow_afac', 'dateNow_afac1'));
-        return view('livewire.medicine.medicine-afac.home-medicine-afac' , compact('date_afac1', 'date2_afac1', 'date1_afac1', 'tomorrow_afac1', 'dateNow_afac1'));
+        return view('livewire.medicine.medicine-afac.home-medicine-afac' , compact('date2_afac1'));
     }
 }
