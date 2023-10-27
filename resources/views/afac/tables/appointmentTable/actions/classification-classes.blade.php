51f4cd20d71567afa94221eaf4b467e300d9879a
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-10-27 13:39:12 -0600

    se añade el isset

--- resources/views/afac/tables/appointmentTable/actions/classification-classes.blade.php
+++ resources/views/afac/tables/appointmentTable/actions/classification-classes.blade.php
@@ -11,9 +11,10 @@
                 {{ $licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass->name }}
             @endif
         @elseif($licencia->medicineReserveMedicine->medicineTypeExam->id == 4)
-                {{ $licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass->name }}
+                {{-- {{ $licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass->name }} --}}
+                {{ isset($licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass) ? $licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass->name : '' }}
         @elseif($licencia->medicineReserveMedicine->medicineTypeExam->id == 5)
-                {{ $licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass->name }}
+                {{ isset($licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass) ? $licencia->medicineReserveMedicine->medicineRevaluation[0]->revaluationMedicineRenovation[0]->revaluationRenovationClasificationClass->name: ''}}
         @endif
     @endforeach
 </div>
