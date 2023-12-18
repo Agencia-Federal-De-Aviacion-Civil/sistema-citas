79c46a39e693af3b675003a4441dbe27e06bd960
Alberto-AFAC <jmondragonescamilla@gmail.com>
2023-12-18 14:12:43 -0600

    se manda la logica para las citas con status 11

--- resources/views/components/medicine/appointment-actions-component.blade.php
+++ resources/views/components/medicine/appointment-actions-component.blade.php
@@ -119,5 +119,11 @@
         <x-badge flat positive label="CONCLUYÓ APTO" />
     @elseif($status == 9)
         <x-badge flat negative label="CONCLUYÓ NO APTO" />
+    @elseif($status == 11)
+        @hasrole('super_admin|super_admin_medicine')
+        LIBERAR
+        @else
+            <x-badge flat negative label="PENDIENTE" />
+        @endhasrole
     @endif
 </div>
