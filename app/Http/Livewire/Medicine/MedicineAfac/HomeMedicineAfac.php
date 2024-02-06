e86dc746ae3a0d02d5aabd965702ab5135e89778
Alberto-AFAC <jmondragonescamilla@gmail.com>
2024-02-06 16:48:18 -0600

    se toma solo un valor

--- app/Http/Livewire/Medicine/MedicineAfac/HomeMedicineAfac.php
+++ app/Http/Livewire/Medicine/MedicineAfac/HomeMedicineAfac.php
@@ -59,7 +59,7 @@ class HomeMedicineAfac extends Component
                 $headquarters->where('is_external', 1);
             })
             ->get(['id', 'name_headquarter', 'is_external']);
-        $this->headquarterQueries = $headquarters;
+        $this->headquarterQueries = $headquarters->take(1);
 
         $this->appointmentNow = $appointmentDashboard->where('dateReserve', $date1);
         $this->nowDate = ($id_dashboard === 0 || Auth::user()->canany(['headquarters.see.dashboard', 'sub_headquarters.see.dashboard', 'medicine_admin.see.dashboard'])) ? $this->appointmentNow->whereIn('status', ['0', '1', '4', '10'])->sum('count') : ($id_dashboard === 1 || Auth::user()->can('headquarters_authorized.see.dashboard') ? $this->appointmentNow->whereIn('status', ['0', '1', '4', '10', '7', '8', '9'])->sum('count') : null);
