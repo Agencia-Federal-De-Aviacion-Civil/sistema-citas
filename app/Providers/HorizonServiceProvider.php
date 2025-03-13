47f6af8110d67e10b272fd11a210221d937428fe
Alberto-AFAC <jmondragonescamilla@gmail.com>
2025-03-13 12:33:30 -0600

    se manda horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                'admin@afac.gob.mx'
+                'admin1@afac.gob.mx'
             ]);
         });
     }
